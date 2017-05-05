e0f4142df57abc24cbbdf85b1d201dd441246af6
jonasLupu <jonas_lupu@hotmail.fr>
2017-05-05 16:32:45 +0200

    Ajout capteur
    Il faut rajouter le header à la page

---
+++ Capteur.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="StyleCapteur.css" />
+        <title>Formulaire Capteur</title>
+    </head>
+
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    <body>
+    <h1> Ajouter un nouveau capteur </h1>
+
+		<p class = Formulaire>
+		<input type="text" name="Type de capteur" placeholder="Type de capteur" size=70/>
+		</p>
+		<p class = Formulaire>
+		<input type="text" name="Numéro de série" placeholder="Numéro de série" size=70/>
+		</p>	
+		<button class="valider" type=submit> Valider </button>
+	</body>
+    </body>
+</html>
