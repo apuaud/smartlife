59fbe01bd02d9bc43e301e43fd26941b1ade40a6
jonasLupu <jonas_lupu@hotmail.fr>
2017-05-05 16:58:45 +0200

    Header (de la part de Guillaume, Basile et Jonas)
    Il faut mettre une balise php pour l’intégrer en haut de chaque page.

---
+++ TestHeader.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="StyleHeader.css" />
+		<title>Test Formulaires</title>
+	</head>
+			<header>
+			<img src="Images/logo.png" label="logo">
+			<section class=ContenuHeader> 	
+				 <section class=MenuHeader> 
+					<table>
+						<tr class=tableHeader> 
+							<td class=ongletHeader> Accueil</td>
+							<td class=ongletHeader> Présentation</td>
+							<td class=ongletHeader> Contacts</td>
+						</tr>
+					</table>
+					<section class=ongletCoHeader> 
+						<p class=titreConnecté> Vous êtes connectés </p>
+						<button class=deconnexion type=submit> Se déconnecter </button>
+					</section>	
+				</section>
+					<p class = titreEspace> Votre espace personnel </p>
+			</section>
+		</header>
