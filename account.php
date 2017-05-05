8975017dc88d45216cb4df0279b3f41b25a12a88
apuaud <adrien@MacBook-Pro-de-Adrien.local>
2017-05-05 16:58:38 +0200

    Liaision BDD / liste maison

--- account.php
+++ account.php
@@ -1,4 +1,91 @@
 <?php
 session_start();
+include("db_connect.php");
 ?>
-<p>Ajouter une <a href="add_house.php">nouvelle maison</a></p>
+<!DOCTYPE html>
+	<html>
+
+		<head>
+
+			<meta charset="utf-8"/>
+			<link rel="stylesheet" href="styleespaceperso.css"/>
+			<title>Mon espace personnel</title>
+		</head>
+		<body>
+		<header>
+		<table align="center">
+		<tr>
+			<td><p class="textbox2">Votre espace personnel</p></td>
+		</tr>
+		</table>
+		</header>
+
+
+			<table class="listepiece">
+				<tr>
+				<?php
+				$reponse = $dbh->query('SELECT nom 
+					FROM logement,users_logement 
+					WHERE users_logement.id_user=\'' . $_SESSION['id'] . '\'
+					AND logement.id=users_logement.id_logement');
+
+				while($donnees = $reponse->fetch())
+				{
+					echo "<td><div class='textbox'><span>" . $donnees['nom'] . "</span></div></td>";
+				} ?>
+					<td><div class="textbox">
+					<span><a href="add_house.php">Ajouter une nouvelle maison</a></span>
+					</td>	
+				</tr>
+			</table>
+			<?php $reponse->closeCursor(); ?>
+			<table>
+				<tr>
+					<td><div class="textbox dropdown">
+					<span>Salon</span>
+					<div class="texbox dropdown-content">
+				<ul>
+					<div class="liste"><li>Température</li>
+					<li>Luminosité</li>
+					<li>Volets</li>
+					<li>Humidité</li></div>
+					<li class="plus">+</li>
+				</ul>
+					</div>
+					</div>
+					</td>
+
+					<td><div class="textbox dropdown">
+					<span>Cuisine</span>
+					<div class="texbox dropdown-content">
+				<ul>
+					<div class="liste"><li>Température</li>
+					<li>Luminosité</li>
+					<li>Volets</li>
+					<li>Humidité</li></div>
+					<li class="plus">+</li>
+					</div>
+					</div>
+					</td>
+				</ul>
+					<td><div class="textbox dropdown">
+					<span>Chambres</span>
+					<div class="texbox dropdown-content">
+				<ul>
+					<div class="liste"><li>Température</li>
+					<li>Luminosité</li>
+					<li>Volets</li>
+					<li>Humidité</li></div>
+					<li class="plus">+</li>
+				</ul>
+					</div>
+					</div>
+					</td>
+					<td><div class="textbox dropdown">
+					<span>+</span>
+					</div>
+					</td>
+				</tr>
+		</table>		
+	</body>
+</html>
