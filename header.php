589bf047e9a28d005ab362223b1d7f5436570f7b
Absetou <syabsetou@gmail.com>
2017-05-12 15:36:58 +0200

    header

---
+++ header.php
@@ -0,0 +1,20 @@
+<div class="container" id="accueil" >
+		
+		<p id="logoContainer"><img align="middle"id="logo2" src="img/accueil_logo.png"></p>
+			<table id = "barreAccueil" class="caption">
+				<tr>
+					<td id="itemAccueil" class="menuItem">Accueil</td>
+					<td id="itemPresentation" class="menuItem">Présentation</td>
+					<td id='itemLogo' ></td>
+					<td id='itemAccount'class="menuItem" onclick="displayFormulaire()">Espace personnel</td>
+					<td id='itemAide'class="menuItem"><a href="/app/contact.php" style="text-decoration:none;color:inherit;">Support</a></td>
+				</tr>
+				<!--<tr id="ligneServices">
+					<td></td>
+					<td id="levelServices"></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>-->
+			</table>
+</div>
