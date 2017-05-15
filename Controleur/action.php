9879a069df86a93b7f457938a84b4d5310ebe662
Julien Perret <julienperret01@gmail.com>
2017-05-15 12:05:30 +0200

    Action Links solved

--- Controleur/action.php
+++ Controleur/action.php
@@ -6,63 +6,63 @@ if(ISSET($_GET['action']))
   switch($action)
   {
     case "connexion" :
-      include("connexion.php");
+      include("../connexion.php");
       break;
 
     case "goToOublieMotDePasse" :
-      include("lostpassword.php");
+      include("../lostpassword.php");
       break;
 
     case "goToInscription" :
-      include("register.php");
+      include("../register.php");
       break;
 
     case "validerInscription" :
-      include("inscription.php");
+      include("../inscription.php");
       break;
 
     case "goToLanguefr" :
-      include("Vue/Index/languefr.php");
+      include("../Vue/Index/languefr.php");
       break;
 
     case "goToLangueen" :
-      include("Vue/Index/langueen.php");
+      include("../Vue/Index/langueen.php");
       break;
 
     case "goToSupport" :
-      include("Vue/Support/support.php");
+      include("../Vue/Support/support.php");
       break;
 
     case "goToAjoutMaison" :
-      include("Vue/EsapcePerso/add_house.php");
+      include("../Vue/EsapcePerso/add_house.php");
       break;
 
     case "validerAjoutMaison" :
-      include("ajouter_maison.php");
+      include("../ajouter_maison.php");
       break;
 
     case "goToAjoutPiece" :
-      include("Vue/EspacePerso/add_room.php");
+      include("../Vue/EspacePerso/add_room.php");
       break;
 
     case "validerAjoutPiece" :
-      include("ajouter_piece.php");
+      include("../ajouter_piece.php");
       break;
 
     case "goToAjoutCapteur" :
-      include("Vue/EspacePerso/add_capteur.php");
+      include("../Vue/EspacePerso/add_capteur.php");
       break;
 
     case "validerAjoutCapteur" :
-      include("ajouter_capteur.php");
+      include("../ajouter_capteur.php");
       break;
 
     case "goToParametre" :
-      include("parametre.php");
+      include("../parametre.php");
       break;
 
     case "sendMail" :
-      include("mail.php");
+      include("../mail.php");
       break;
   }
 ?>
